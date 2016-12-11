10514230a14b73d5322d5c19525f76ed36645a59
skramakant <ramakant.singh17@gmail.com>
2016-12-11 13:19:07 +0530

    port change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var privateKey  = fs.readFileSync(__dirname+'/key.pem', 'utf8');
 var certificate = fs.readFileSync(__dirname+'/cert.pem', 'utf8');
 
 var server_ip_address  = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3001;
 
 var checkMimeType = true;
 var credentials = {key: privateKey, cert: certificate};
